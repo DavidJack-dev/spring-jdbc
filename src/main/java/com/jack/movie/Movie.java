57bdbd58ef1598fe44a222cf3761178849d93796
DavidJakc-dev <youngsik3200@gmail.com>
2022-04-04 15:44:25 +0900

    jdbc test

---
+++ src/main/java/com/jack/movie/Movie.java
@@ -0,0 +1,12 @@
+package com.jack.movie;
+
+import com.jack.actor.Actor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record Movie(Integer id,
+                    String name,
+                    List<Actor> actors,
+                    LocalDate releaseDate) {
+}
---
+++ src/main/java/com/jack/movie/MovieDataAccessService.java
@@ -0,0 +1,47 @@
+package com.jack.movie;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MovieDataAccessService implements MovieDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public MovieDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    @Override
+    public List<Movie> selectMovies() {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public int insertMovie(Movie movie) {
+        String sql = """
+                INSERT INTO movie(name, release_date) 
+                VALUES (?, ?);
+                    """;
+        return jdbcTemplate.update(
+                sql,
+                movie.name(), movie.releaseDate());
+    }
+
+    @Override
+    public int deleteMovie(int id) {
+        throw new UnsupportedOperationException("not implemented");
+
+    }
+
+    @Override
+    public Optional<Movie> selectMovieById(int id) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+}
